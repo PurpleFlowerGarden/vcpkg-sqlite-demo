1ae121c53e078a7e918edf6fc456b40add42ca60
Schaich Alonso <alonsoschaich@fastmail.fm>
2023-05-27 17:41:52 +0000

    add -v:n as proposed in microsoft/vcpkg#31659

--- build.ps1
+++ build.ps1
@@ -6,5 +6,5 @@ cmake -E make_directory ${env:GITHUB_WORKSPACE}/build
 
 pushd ${env:GITHUB_WORKSPACE}/build
 	cmake -DCMAKE_TOOLCHAIN_FILE="${env:GITHUB_WORKSPACE}/vcpkg/scripts/buildsystems/vcpkg.cmake" ..
-	cmake --build . --config Release
+	cmake --build . --config Release -- -v:n
 popd
