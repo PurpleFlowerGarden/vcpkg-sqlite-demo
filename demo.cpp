8c167a69a2820e1df72a3a89a7490347f7168e9c
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-03-06 08:04:10 +0900

    Try using glib

--- demo.cpp
+++ demo.cpp
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <sqlite3.h>
+#include <glib.h>
 
 int main() {
-	printf("Linked Against SQLite3 Version: %s\n", sqlite3_libversion());
 	return EXIT_SUCCESS;
 }
